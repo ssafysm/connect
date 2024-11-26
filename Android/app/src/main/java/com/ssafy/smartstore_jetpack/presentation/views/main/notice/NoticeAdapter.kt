1c2ad16640c50b592dd6a66bc94c8ce532d6785f
sungmin <ssafysm@gmail.com>
2024-11-26 15:30:28 +0900

    commit

--- Android/app/src/main/java/com/ssafy/smartstore_jetpack/presentation/views/main/notice/NoticeAdapter.kt
+++ Android/app/src/main/java/com/ssafy/smartstore_jetpack/presentation/views/main/notice/NoticeAdapter.kt
@@ -8,34 +8,44 @@ import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.smartstore_jetpack.databinding.ListItemNoticeBinding
 import com.ssafy.smartstore_jetpack.domain.model.Alarm
 import com.ssafy.smartstore_jetpack.presentation.views.main.MainViewModel
+import io.noties.markwon.Markwon
 
 class NoticeAdapter(private val viewModel: MainViewModel) :
     ListAdapter<Alarm, RecyclerView.ViewHolder>(diffUtil) {
 
-    class NoticeViewHolder(private val binding: ListItemNoticeBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    private lateinit var markwon: Markwon
+
+    class NoticeViewHolder(
+        private val binding: ListItemNoticeBinding,
+        private val markwon: Markwon
+    ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(alarm: Alarm, viewModel: MainViewModel) {
-            binding.alarm = alarm
             binding.vm = viewModel
+            binding.alarm = alarm
+            // Markwon을 사용하여 Markdown 처리
+            markwon.setMarkdown(binding.tvMessageNotify, alarm.content)
+            binding.executePendingBindings()
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoticeViewHolder =
-        NoticeViewHolder(
-            ListItemNoticeBinding.inflate(
-                LayoutInflater.from(parent.context), parent, false
-            )
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoticeViewHolder {
+        // Markwon 인스턴스를 생성 (Context 필요)
+        if (!::markwon.isInitialized) {
+            markwon = Markwon.create(parent.context)
+        }
+        val binding = ListItemNoticeBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
         )
+        return NoticeViewHolder(binding, markwon)
+    }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         (holder as NoticeViewHolder).bind(currentList[position], viewModel)
     }
 
     companion object {
-
         val diffUtil = object : DiffUtil.ItemCallback<Alarm>() {
-
             override fun areContentsTheSame(oldItem: Alarm, newItem: Alarm): Boolean =
                 (oldItem == newItem)
 
